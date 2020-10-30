8209ebf752d044261ed0c7e1705c00422cfed409
niayigoji <ioeory@gmail.com>
2020-10-30 13:13:01 +0800

    Create get_netid.js

---
+++ get_netid.js
@@ -0,0 +1,6 @@
+if(window.jQuery) {
+var $cell = jQuery('td.mapsearchOuterCell');
+var netIds = [];
+jQuery('td.mapsearchOuterCell').each((index, cell) => {netIds.push($(cell).children('a').data('netid'))});
+console.log(netIds.join('\n'))
+}
